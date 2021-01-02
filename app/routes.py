2dbc9c72f42741c9054f35e69f09d83b4a688b25
jkelly2 <jkelly2@illumina.com>
2021-01-02 14:55:33 +0000

    logging.info(f'Results will be saved in ') Chapter three changes and start of chapter 4

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +19,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts = posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for {form.username.data}, remember_me={form.remember_me.data},'
+              f' password = {form.password.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
